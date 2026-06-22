23ef213ae86d2f23ec885b4c644e1c256bd87bb1
trista-xuuu <trista10418063@gmail.com>
2026-06-22 21:47:46 +0800

    fix: hardcode sanity public projectId as fallback

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,6 +1,6 @@
 import { defineCliConfig } from "sanity/cli";
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "uoccppjd";
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
 
 export default defineCliConfig({ api: { projectId, dataset } });
--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,6 +1,6 @@
 import { createClient } from "next-sanity";
 
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "uoccppjd";
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
 export const apiVersion = "2024-01-01";
 
